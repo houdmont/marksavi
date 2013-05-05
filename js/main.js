a8d86315fbf497b75eb277b7774a7f523f72d11f
Tom Houdmont <tomhoudmont@gmail.com>
2013-05-05 15:42:16 +0100

    Jquery added, and navigation animation implemented

--- js/main.js
+++ js/main.js
@@ -1,57 +1,28 @@
-// CAN BE GREATLY IMPROVED
-// 
-// Just make one function (slideTo) which will slide the bar to an element
-// Give it the hovered element on hover, and the active element on leaving
-// hover. If there is no active element, just have it set to display: none.
-// 
-// Default css styling for it should include position:absolute and a top 
-// value.
-
 $(function() {
-    // Find the #nav_slider
-    $ns = $('#nav_slider');
 
-    // Find the navigation items
-    $nav_items = $('#nav a');
+    var underline  = $('.underline'),
+        links      = $('.navigation li'),
+        activeLink = $('.navigation .active'),
+        moveTo     = function(element) {
 
-    var backToActive = function(elem) {
+            // Grab various information about the element.
+            var el  = $(element),
+                pos = el.position(),
+                w   = el.outerWidth();
 
-        $this = elem;
+            // Stop the current underline animation.
+            underline.stop();
 
-        // Find the active element.
-        $active = $nav_items.filter('.active');
+            // Animate the underline to the element.
+            underline.animate({
+                width: w,
+                left: pos.left + 'px'
+            }, 1000, 'easeOutBack');
+        };
 
-        var pos = $active.position();
-        var w   = $active.outerWidth();
-
-        $ns.animate({
-            width: w,
-            left: pos.left + 'px'
-        }, 1000, 'easeOutBack');
-
-    };
-
-    $nav_items.hover(function() {
-
-        // On hover
-        $ns.stop();
-
-        // Find position of item.
-        $this = $(this);
-        var pos = $this.position();
-        var w = $this.outerWidth();
-
-        $ns.animate({
-            width: w,
-            left: pos.left + 'px'
-        }, 1000, 'easeOutBack');
-
-    }, function() {
-        // Off hover.
-        $ns.stop();
-
-        backToActive($(this));
-    });
-
-    backToActive($ns);
+        links.hover(function(evt) {
+            moveTo(this);
+        }, function(evt) {
+            moveTo(activeLink);
+        });
 });
