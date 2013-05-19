8c12bc5adb926f8434c04a8cebb6b7250129e1a5
Tom Houdmont <tomhoudmont@gmail.com>
2013-05-19 22:23:34 +0100

    Scroll spy like functionality added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 $(function() {
 
     var underline  = $('.underline'),
-        links      = $('.navigation li'),
-        activeLink = $('.navigation .active'),
-        moveTo     = function(element) {
+        listitems  = $('.navigation li'),
+        links      = $('.navigation a'),
+        sections   = $('#whenwhere, #rsvp, #bestman, #maidhonour, #gifts, #photo'),
+        doc        = document.documentElement,
+        body       = document.body,
+        positions  = [],
+        moveTo     = function(element, activate) {
 
             // Grab various information about the element.
             var el  = $(element),
                 pos = el.position(),
                 w   = el.outerWidth();
+
+            if(activate) {
+                listitems.removeClass('active');
+                el.addClass('active');
+            }
 
             // Stop the current underline animation.
             underline.stop();
@@ -20,11 +29,36 @@ $(function() {
             }, 1000, 'easeOutBack');
         };
 
-        links.hover(function(evt) {
-            moveTo(this);
+        listitems.hover(function(evt) {
+            moveTo(this, false);
         }, function(evt) {
-            moveTo(activeLink);
+            moveTo(listitems.filter('.active'), false);
         });
 
-        // moveTo(activeLink);
+        links.on('click', function(evt) {
+            evt.preventDefault();
+
+            // Retrieve the position of the target element.
+            var target = this.href.split('#')[1],
+                elem   = $('#' + target),
+                pos    = elem.position();
+
+            listitems.removeClass('active');
+            links.filter('#' + target).parent().addClass('active');
+
+            // Animate the window down to that position.
+            $('body, html').animate({
+                scrollTop: pos.top - 52
+            }, 800, 'easeOutSine');
+        });
+
+        sections.on('inview', function(evt, isInView, visiblePartX, visiblePartY) {
+            
+            if(isInView && visiblePartY == 'top') {
+                var href = '#' + evt.target.id;
+                var link = links.filter('a[href=' + href + ']');
+                var item = link.parent();
+                moveTo(item, true);
+            }
+        });
 });
