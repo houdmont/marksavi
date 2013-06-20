bee3edb74bbd74e38a3f455426efead31e59e728
Tom Houdmont <tomhoudmont@gmail.com>
2013-06-20 17:44:34 +0100

    New images and tooltipster content

--- js/main.js
+++ js/main.js
@@ -29,36 +29,67 @@ $(function() {
             }, 1000, 'easeOutBack');
         };
 
-        listitems.hover(function(evt) {
-            moveTo(this, false);
-        }, function(evt) {
-            moveTo(listitems.filter('.active'), false);
-        });
+    listitems.hover(function(evt) {
+        moveTo(this, false);
+    }, function(evt) {
+        moveTo(listitems.filter('.active'), false);
+    });
 
-        links.on('click', function(evt) {
-            evt.preventDefault();
+    links.on('click', function(evt) {
+        evt.preventDefault();
 
-            // Retrieve the position of the target element.
-            var target = this.href.split('#')[1],
-                elem   = $('#' + target),
-                pos    = elem.position();
+        // Retrieve the position of the target element.
+        var target = this.href.split('#')[1],
+            elem   = $('#' + target),
+            pos    = elem.position();
 
-            listitems.removeClass('active');
-            links.filter('#' + target).parent().addClass('active');
+        listitems.removeClass('active');
+        links.filter('#' + target).parent().addClass('active');
 
-            // Animate the window down to that position.
-            $('body, html').animate({
-                scrollTop: pos.top - 52
-            }, 800, 'easeOutSine');
-        });
+        // Animate the window down to that position.
+        $('body, html').animate({
+            scrollTop: pos.top - 52
+        }, 800, 'easeOutSine');
+    });
 
-        sections.on('inview', function(evt, isInView, visiblePartX, visiblePartY) {
-            
-            if(isInView && visiblePartY == 'top') {
-                var href = '#' + evt.target.id;
-                var link = links.filter('a[href=' + href + ']');
-                var item = link.parent();
-                moveTo(item, true);
-            }
-        });
+    sections.on('inview', function(evt, isInView, visiblePartX, visiblePartY) {
+        
+        if(isInView && visiblePartY == 'top') {
+            var href = '#' + evt.target.id;
+            var link = links.filter('a[href=' + href + ']');
+            var item = link.parent();
+            moveTo(item, true);
+        }
+    });
+
+    $('.tooltipster').tooltipster({
+        animation: 'grow',
+        arrow: true,
+        arrowColor: '',
+        content: '',
+        delay: 50,
+        fixedWidth: 0,
+        maxWidth: 0,
+        functionBefore: function(origin, continueTooltip) {
+          continueTooltip();
+        },
+        functionReady: function(origin, tooltip) {},
+        functionAfter: function(origin) {},
+        icon: '(?)',
+        iconDesktop: false,
+        iconTouch: false,
+        iconTheme: '.tooltipster-icon',
+        interactive: false,
+        interactiveTolerance: 350,
+        offsetX: 0,
+        offsetY: 0,
+        onlyOne: true,
+        position: 'top',
+        speed: 350,
+        timer: 0,
+        theme: '.tooltipster-shadow',
+        touchDevices: true,
+        trigger: 'hover',
+        updateAnimation: true 
+    });
 });
